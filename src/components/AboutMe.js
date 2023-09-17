45245d47a22955041394349b01b08a0922c9473e
Jeff McMillen <jmcmillenmusic@gmail.com>
2023-09-17 18:19:36 -0500

    Re-removed Bulma, added images and projectList for Portfolio.js

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,14 +6,14 @@ function AboutMe() {
         <section className="aboutMe">
             <br />
             <img src={img} alt="Jeff McMillen's Headshot" className="card border1"></img>
-                <div className="bio">
-                    <h2 className="hex1 border1">
-                        About Me
-                    </h2>
-                    <p className="hex1 border1">
-                        I'm Jeff McMillen, a web developer in Austin, TX who previously worked in game audio for the last 10 years. In this new career path, my vocational goal is to use code to solve people's problems. When I'm not working on code, you can find me walking, hiking, playing disc golf, playing all kinds of games (board, card, computer, and video games), reading, and spending time with friends and family.
-                    </p>
-                </div>
+            <div className="bio">
+                <h2 className="hex1 border1 is-size-2">
+                    About Me
+                </h2>
+                <p className="hex1 border1">
+                    I'm Jeff McMillen, a web developer in Austin, TX who previously worked in game audio for the last 10 years. In this new career path, my vocational goal is to use code to solve people's problems. When I'm not working on code, you can find me walking, hiking, playing disc golf, playing all kinds of games (board, card, computer, and video games), reading, and spending time with friends and family.
+                </p>
+            </div>
         </section>
     )
 };
