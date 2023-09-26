0b7d0854779db4aaa81722c2dc3d9e96bb8f52ad
Jeff McMillen <jmcmillenmusic@gmail.com>
2023-09-26 18:05:52 -0500

    Added descriptions and technologies to Portfolio section

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -18,36 +18,54 @@ function Portfolio({ projects }) {
                         Chronoscape
                     </h3>
                     <a href="https://github.com/jmcmillenmusic/chronoscape" target="_blank" rel="noopener noreferrer"><img src={projImg1} alt="Open portal with lights streaming out from it" className="hex2 border1"></img></a>
+                    <p className="hex2 border1">Choose-your-own-adventure game where you are a Multiverse Protection Force (MPF) agent sent to track down another MPF agent who has gone rogue
+                    <br /><br />
+                    <u>Technologies Utilized</u>: Structured Query Language (SQL), Sequelize.js, Handlebars.js, ESLint, JavaScript</p>
                 </div>
                 <div className="projectCard">
                     <h3 className="hex2 border1">
                         CelesTracker
                     </h3>
                     <a href="https://jmcmillenmusic.github.io/celestracker/" target="_blank" rel="noopener noreferrer"><img src={projImg2} alt="Planet Earth with a view of space in the background" className="hex2 border1"></img></a>
+                    <p className="hex2 border1">Application where the user submits their location and the name of a star, planet, or constellation and receives altitude and azimuth coordinates
+                    <br /><br />
+                    <u>Technologies Utilized</u>: Hypertext Markup Language (HTML5), Cascading Style Sheets (CSS3), Bulma, JavaScript, Application Programming Interfaces (APIs)</p>
                 </div>
                 <div className="projectCard">
                     <h3 className="hex2 border1">
                         SQL Employee Tracker
                     </h3>
                     <a href="https://github.com/jmcmillenmusic/sql-employee-tracker" target="_blank" rel="noopener noreferrer"><img src={projImg3} alt="Table of employees with names, titles, salaries, and managers" className="hex2 border1"></img></a>
+                    <p className="hex2 border1">Back-end application where a user can view or add departments, roles, and employees as well as update an employee's role
+                    <br /><br />
+                    <u>Technologies Utilized</u>: Node.js, Express.js, SQL, Inquirer, JavaScript</p>
                 </div>
                 <div className="projectCard">
                     <h3 className="hex2 border1">
                         NoSQL Social Network API
                     </h3>
                     <a href="https://github.com/jmcmillenmusic/nosql-social-network-api" target="_blank" rel="noopener noreferrer"><img src={projImg4} alt="Screenshot of a thought-provoking question and answer" className="hex2 border1"></img></a>
+                    <p className="hex2 border1">Back-end application where a user can create, read, update, and delete users, thoughts, and reactions (similar to posts and comments on social media)
+                    <br /><br />
+                    <u>Technologies Utilized</u>: Node.js, Express.js, MongoDB, Insomnia, JavaScript</p>
                 </div>
                 <div className="projectCard">
                     <h3 className="hex2 border1">
                         ORM E-Commerce Back-End
                     </h3>
                     <a href="https://github.com/jmcmillenmusic/orm-e-commerce-back-end" target="_blank" rel="noopener noreferrer"><img src={projImg5} alt="List of functions for updating inventory for an e-commerce site" className="hex2 border1"></img></a>
+                    <p className="hex2 border1">Tech demo that utilizes application programming interface routes to create, read, update, and delete data for an online store
+                    <br /><br />
+                    <u>Technologies Utilized</u>: Node.js, Express.js, Sequelize.js, Insomnia, Object-Relational Mapping (ORM), JavaScript</p>
                 </div>
                 <div className="projectCard">
                     <h3 className="hex2 border1">
                         MVC Tech Blog
                     </h3>
                     <a href="https://jeff-mcmillen-tech-blog-f37a90b6c5d9.herokuapp.com/" target="_blank" rel="noopener noreferrer"><img src={projImg6} alt="Tech Blog with a list of tech-related posts and a navbar at the top" className="hex2 border1"></img></a>
+                    <p className="hex2 border1">Tech blog where users can create an account, log in, see all of their posts, create new posts and comment on other posts, update their posts, and delete their posts
+                    <br /><br />
+                    <u>Technologies Utilized</u>: Node.js, Express.js, Sequelize.js, SQL, Handlebars.js, JavaScript</p>
                 </div>
             </div>
         </section>
