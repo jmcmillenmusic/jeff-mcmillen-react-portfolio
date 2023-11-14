62d2e93859ac31f86b4205e7630b8939bca11b87
Jeff McMillen <jmcmillenmusic@gmail.com>
2023-11-14 10:08:12 -0600

    Updated Resume file to v.1.2, updated About Me section and skills in Resume section

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,19 +1,19 @@
 // Imports React, the resume icon, and the resume
 import React from "react";
 import img from "../images/resume-icon.png";
-import resume from "../files/McMillen_Jeff_WebDev_Resume_v.1.0.pdf";
+import resume from "../files/McMillen_Jeff_WebDev_Resume_v.1.2.pdf";
 
 // Establishes arrangement of HTML for Resume component complete with a list of skills and a link to download the resume
 function Resume() {
     return (
         <section className="resume" id="resume">
-            <a href="../files/McMillen_Jeff_WebDev_Resume_v.1.0.pdf" download={resume} target="_blank"><img src={img} alt="Icon for a resume" className="resumeIcon border1"></img></a>
+            <a href="../files/McMillen_Jeff_WebDev_Resume_v.1.2.pdf" download={resume} target="_blank"><img src={img} alt="Icon for a resume" className="resumeIcon border1"></img></a>
             <div className="skills">
                 <h2 className="hex4 border1">
                     Resume
                 </h2>
                 <p className="hex4 border1">
-                    <em>Skills: Hypertext Markup Language (HTML5), Cascading Style Sheets (CSS3), JavaScript, GitHub, jQuery, Bootstrap, Bulma, Application Programming Interfaces (APIs), Node.js, Express.js, Insomnia, Object-Oriented Programming (OOP), Jest, Inquirer, Object-Relational Mapping (ORM), RESTful APIs, Structured Query Language (SQL), MySQL, Sequelize.js, Handlebars.js, ESLint, MongoDB, Progressive Web Applications (PWAs), React.js, Apollo GraphQL</em>
+                    <em>Skills: Hypertext Markup Language (HTML5), Cascading Style Sheets (CSS3), JavaScript, GitHub, jQuery, Bootstrap, Bulma, Application Programming Interfaces (APIs), Node.js, Express.js, Insomnia, Object-Oriented Programming (OOP), Jest, Inquirer, Object-Relational Mapping (ORM), RESTful APIs, Structured Query Language (SQL), MySQL, Sequelize.js, Handlebars.js, Model-View-Controller (MVC) Architecture, ESLint, MongoDB, Progressive Web Applications (PWAs), React.js, Apollo GraphQL</em>
                 </p>
             </div>
         </section>
