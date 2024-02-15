0b15241d26a7217da235c36b4a270a0726d974c8
Jeff McMillen <jmcmillenmusic@gmail.com>
2024-02-15 15:39:55 -0600

    Removed 3 projects from Portfolio, changed hexes to boxes when viewing site on mobile

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,20 +1,20 @@
 // Imports React and image sources for the 6 images to be used in the Portfolio's project cards
 import React from "react";
-import projImg1 from '../images/chronoscape.jpg';
-import projImg2 from '../images/earth.jpg';
+// import projImg1 from '../images/chronoscape.jpg';
+// import projImg2 from '../images/earth.jpg';
 import projImg3 from '../images/emp-tracker.png';
 import projImg4 from '../images/social-network-api.png';
-import projImg5 from '../images/ecommerce.png';
+// import projImg5 from '../images/ecommerce.png';
 import projImg6 from '../images/tech-blog.png';
 import github from "../images/github.png";
 
 // Establishes arrangement of HTML for Portfolio component with cards showing names of projects and links embedded in images
-function Portfolio({ projects }) {
+function Portfolio() {
     return (
         <section className="portfolio" id="portfolio">
             <h2 className="hex2 border1">Portfolio</h2>
             <div className="project-list">
-                <div className="projectCard">
+                {/* <div className="projectCard">
                     <a href="https://chronoscape-g3-dd74e8283e15.herokuapp.com/" target="_blank" rel="noopener noreferrer">
                         <h3 className="hex2 border1">
                             Chronoscape
@@ -29,8 +29,8 @@ function Portfolio({ projects }) {
                         <img src={github} alt="Blue circle GitHub Logo" className="icon"/>
                     </a>
                     </p>
-                </div>
-                <div className="projectCard">
+                </div> */}
+                {/* <div className="projectCard">
                     <a href="https://jmcmillenmusic.github.io/celestracker/" target="_blank" rel="noopener noreferrer">
                         <h3 className="hex2 border1">
                             CelesTracker
@@ -42,6 +42,22 @@ function Portfolio({ projects }) {
                     <em>Technologies Utilized: Hypertext Markup Language (HTML5), Cascading Style Sheets (CSS3), Bulma, JavaScript, Application Programming Interfaces (APIs)</em>
                     <br /><br />
                     <a href="https://github.com/jmcmillenmusic/celestracker" target="_blank" rel="noopener noreferrer">
+                        <img src={github} alt="Blue circle GitHub Logo" className="icon"/>
+                    </a>
+                    </p>
+                </div> */}
+                <div className="projectCard">
+                    <a href="https://jeff-mcmillen-tech-blog-f37a90b6c5d9.herokuapp.com/" target="_blank" rel="noopener noreferrer">
+                        <h3 className="hex2 border1">
+                            MVC Tech Blog
+                        </h3>
+                    </a>
+                    <img src={projImg6} alt="Tech Blog with a list of tech-related posts and a navbar at the top" className="hex2 border1 screenshot"></img>
+                    <p className="hex2 border1">Tech blog where users can create an account, log in, see all of their posts, create new posts and comment on other posts, update their posts, and delete their posts
+                    <br /><br />
+                    <em>Technologies Utilized: Node.js, Express.js, Sequelize.js, SQL, Handlebars.js, JavaScript</em>
+                    <br /><br />
+                    <a href="https://github.com/jmcmillenmusic/mvc-tech-blog" target="_blank" rel="noopener noreferrer">
                         <img src={github} alt="Blue circle GitHub Logo" className="icon"/>
                     </a>
                     </p>
@@ -78,7 +94,7 @@ function Portfolio({ projects }) {
                     </a>
                     </p>
                 </div>
-                <div className="projectCard">
+                {/* <div className="projectCard">
                     <a href="https://drive.google.com/file/d/1bMyKwr81L5aw-iZ_WQa4Se1jPRnwy48R/view" target="_blank" rel="noopener noreferrer">
                         <h3 className="hex2 border1">
                             ORM E-Commerce Back-End
@@ -93,23 +109,7 @@ function Portfolio({ projects }) {
                         <img src={github} alt="Blue circle GitHub Logo" className="icon"/>
                     </a>
                     </p>
-                </div>
-                <div className="projectCard">
-                    <a href="https://jeff-mcmillen-tech-blog-f37a90b6c5d9.herokuapp.com/" target="_blank" rel="noopener noreferrer">
-                        <h3 className="hex2 border1">
-                            MVC Tech Blog
-                        </h3>
-                    </a>
-                    <img src={projImg6} alt="Tech Blog with a list of tech-related posts and a navbar at the top" className="hex2 border1 screenshot"></img>
-                    <p className="hex2 border1">Tech blog where users can create an account, log in, see all of their posts, create new posts and comment on other posts, update their posts, and delete their posts
-                    <br /><br />
-                    <em>Technologies Utilized: Node.js, Express.js, Sequelize.js, SQL, Handlebars.js, JavaScript</em>
-                    <br /><br />
-                    <a href="https://github.com/jmcmillenmusic/mvc-tech-blog" target="_blank" rel="noopener noreferrer">
-                        <img src={github} alt="Blue circle GitHub Logo" className="icon"/>
-                    </a>
-                    </p>
-                </div>
+                </div> */}
             </div>
         </section>
     )
